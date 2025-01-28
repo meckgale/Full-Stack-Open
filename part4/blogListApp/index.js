382a67b31bd5944d39b1759e88592efc5c3ead19
meckgalen <meckgalen@gmail.com>
2025-01-28 22:10:08 +0300

    app.js seperated from index.js, app is now refactored and part4.2 is completed

--- part4/blogListApp/index.js
+++ part4/blogListApp/index.js
@@ -1,29 +1,7 @@
-require("dotenv").config();
+const app = require("./app");
 const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
-const mongoose = require("mongoose");
 
-logger.info("connecting to", config.MONGODB_URI);
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("connected to MongoDB");
-  })
-  .catch((error) => {
-    logger.error("error connecting to MongoDB:", error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/blogs", blogsRouter);
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
