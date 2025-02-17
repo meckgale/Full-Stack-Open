e6a1ce7a9b178a91e16e4c3f231f7e9ca0f44689
meckgalen <meckgalen@gmail.com>
2025-02-18 01:19:59 +0300

    part5 exercises 5.1-5.4 are completed

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,168 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import NotificationError from './components/NotificationError'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import NotificationSuccess from './components/NotificationSuccess'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem(
+      'loggedBlogListAppppUser'
+    )
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const sendPost = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url,
+    }
+
+    blogService.create(blogObject).then((returnedPost) => {
+      setBlogs(blogs.concat(returnedPost))
+      setSuccessMessage(`a new blog ${title} by ${author} added`)
+      setTimeout(() => {
+        setSuccessMessage(null)
+      }, 5000)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    })
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogListAppppUser',
+        JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong username or password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <h2>log in to application</h2>
+      <NotificationError message={errorMessage} />
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogForm = () => (
+    <form onSubmit={sendPost}>
+      <div>
+        title:
+        <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+        <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+        <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+
+  return (
+    <div>
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <h2>blogs</h2>
+          <NotificationSuccess message={successMessage} />
+          <p>
+            {user.name} logged-in{' '}
+            <button
+              onClick={() => {
+                window.localStorage.clear()
+                setUser(null)
+              }}
+              type="submit"
+            >
+              logout
+            </button>
+          </p>
+          {blogForm()}
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part5/bloglist-frontend/src/components/NotificationError.jsx
@@ -0,0 +1,9 @@
+const NotificationError = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return <div className="error">{message}</div>
+}
+
+export default NotificationError
