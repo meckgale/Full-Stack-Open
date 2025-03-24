64d4bda04a32c1476bf0e11575c698996be627a3
meckgalen <meckgalen@gmail.com>
2025-03-24 20:49:20 +0300

    part6 section a exercises are completed

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,26 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state)
+  const dispatch = useDispatch()
+
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+  return (
+    <div>
+      {sortedAnecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>
+              vote
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default AnecdoteList
